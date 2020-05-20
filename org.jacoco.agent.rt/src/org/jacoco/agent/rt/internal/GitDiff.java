11a3a502f8fc8ea13184d4a3a2023924e4803f4b
muyi <muyi@gegejia.com>
2020-05-20 21:10:32 +0800

    代码调整

--- org.jacoco.agent.rt/src/org/jacoco/agent/rt/internal/GitDiff.java
+++ org.jacoco.agent.rt/src/org/jacoco/agent/rt/internal/GitDiff.java
@@ -19,92 +19,90 @@ import java.io.IOException;
 import java.util.List;
 
 public class GitDiff {
-	private static Git git;
-	private static Repository repository;
+    private static Git git;
+    private static Repository repository;
 
-	// 获取repository对象--单例DCL模式
-	public static Repository getRepository() {
-		if (null == repository) {
-			synchronized (GitDiff.class) {
-				if (null == repository) {
-					try {
-						String projectPath = System.getProperty("user.dir");
-						repository = new FileRepositoryBuilder()
-								.setGitDir(new File(projectPath + "/.git"))
-								.build();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}
-		return repository;
-	}
+    // 获取repository对象--单例DCL模式
+    public static Repository getRepository() {
+        if (null == repository) {
+            synchronized (GitDiff.class) {
+                if (null == repository) {
+                    try {
+                        String projectPath = System.getProperty("user.dir");
+                        repository = new FileRepositoryBuilder()
+                                .setGitDir(new File(projectPath + "/.git"))
+                                .build();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return repository;
+    }
 
-	// 获取git对象--单例DCL模式
-	public static Git getGit(Repository repository) {
-		if (null == git) {
-			synchronized (GitDiff.class) {
-				if (null == git) {
-					git = new Git(getRepository());
-				}
-			}
-		}
-		return git;
-	}
+    // 获取git对象--单例DCL模式
+    public static Git getGit(Repository repository) {
+        if (null == git) {
+            synchronized (GitDiff.class) {
+                if (null == git) {
+                    git = new Git(getRepository());
+                }
+            }
+        }
+        return git;
+    }
 
-	/**
-	 * @param repository
-	 *            代码仓库
-	 * @param branchName
-	 *            例如"refs/heads/master"
-	 * @return
-	 * @throws IOException
-	 */
-	private static AbstractTreeIterator prepareTreeParserBranch(
-			Repository repository, String branchName) throws IOException {
+    /**
+     * @param repository 代码仓库
+     * @param branchName 例如"refs/heads/master"
+     * @return
+     * @throws IOException
+     */
+    private static AbstractTreeIterator prepareTreeParserBranch(
+            Repository repository, String branchName) throws IOException {
 
-		ObjectId objectId = repository.exactRef(branchName).getObjectId();
+        ObjectId objectId = repository.exactRef(branchName).getObjectId();
 
-		try (RevWalk walk = new RevWalk(repository)) {
-			RevCommit commit = walk.parseCommit(objectId);
-			RevTree tree = walk.parseTree(commit.getTree().getId());
+        try (RevWalk walk = new RevWalk(repository)) {
+            RevCommit commit = walk.parseCommit(objectId);
+            RevTree tree = walk.parseTree(commit.getTree().getId());
 
-			CanonicalTreeParser treeParser = new CanonicalTreeParser();
-			try (ObjectReader reader = repository.newObjectReader()) {
-				treeParser.reset(reader, tree.getId());
-			}
-			walk.dispose();
-			return treeParser;
-		}
-	}
+            CanonicalTreeParser treeParser = new CanonicalTreeParser();
+            try (ObjectReader reader = repository.newObjectReader()) {
+                treeParser.reset(reader, tree.getId());
+            }
+            walk.dispose();
+            return treeParser;
+        }
+    }
 
-	/**
-	 * diff by branch
-	 *
-	 * @param repository
-	 * @param baseBranch
-	 * @param diffBranch
-	 * @throws GitAPIException
-	 * @throws IOException
-	 */
-	public static List<DiffEntry> getDiffByBranch(Repository repository,
-			String baseBranch, String diffBranch) {
-		List<DiffEntry> diffs = null;
-		AbstractTreeIterator baseBranchTree;
-		AbstractTreeIterator diffBranchTree;
-		try {
-			baseBranchTree = prepareTreeParserBranch(repository, baseBranch);
-			diffBranchTree = prepareTreeParserBranch(repository, diffBranch);
+    /**
+     * diff by branch
+     *
+     * @param repository
+     * @param baseBranch
+     * @param diffBranch
+     * @throws GitAPIException
+     * @throws IOException
+     */
+    public static List<DiffEntry> getDiffByBranch(Repository repository,
+                                                  String baseBranch, String diffBranch) {
+        List<DiffEntry> diffs = null;
+        AbstractTreeIterator baseBranchTree;
+        AbstractTreeIterator diffBranchTree;
+        try {
+            baseBranchTree = prepareTreeParserBranch(repository, baseBranch);
+            diffBranchTree = prepareTreeParserBranch(repository, diffBranch);
 
-			diffs = getGit(getRepository()).diff().setOldTree(baseBranchTree)
-					.setNewTree(diffBranchTree)
-					.setPathFilter(PathSuffixFilter.create(".java")).call();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (GitAPIException e) {
-			e.printStackTrace();
-		}
-		return diffs;
-	}
+            diffs = getGit(getRepository()).diff().setOldTree(baseBranchTree)
+                    .setNewTree(diffBranchTree)
+                    .setPathFilter(PathSuffixFilter.create(".java")).call();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+        }
+        return diffs;
+    }
 }
